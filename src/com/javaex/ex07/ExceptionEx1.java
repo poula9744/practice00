1db778cd0e712edfdf028eba082d02b61b9cf238
poula9744 <poula9744@naver.com>
2024-02-09 23:04:19 +0900

    0209

---
+++ src/com/javaex/ex07/ExceptionEx1.java
@@ -0,0 +1,17 @@
+package com.javaex.ex07;
+
+public class ExceptionEx1 {
+
+	public static void main(String[] args) {
+		try {
+			Exception e = new Exception("고의로 발생시켰음.");
+			throw e; //예외를 발생시킴
+			//throw new Exception("고의로 발생시켰음");
+		} catch (Exception e) {
+			System.out.println("에러 메시지: " + e.getMessage());
+			e.printStackTrace();
+		}
+		System.out.println("프로그램이 정상 종료되었음.");
+	}
+
+}
